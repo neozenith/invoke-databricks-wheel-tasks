351ad64520d53714e5fa31d57bb5a5140d1d3a29
Josh Peak <josh.peak@86400.com.au>
2022-03-15 16:25:36 +1100

    Refactor utility functions and update common tasks.

--- invoke_databricks_wheel_tasks/utils/databricks.py
+++ invoke_databricks_wheel_tasks/utils/databricks.py
@@ -4,61 +4,10 @@ import time
 from functools import lru_cache
 from pprint import pprint as pp
 
-# Third Party
-from invoke import run
+from .git import git_current_branch
+from .poetry import poetry_project_name, poetry_wheelname
 
 POLL_DELAY = 5
-
-
-@lru_cache(maxsize=None)
-def git_current_branch():
-    """Get the current git branch."""
-    return run("git branch --show-current", hide=True).stdout.strip()
-
-
-@lru_cache(maxsize=None)
-def poetry_project():
-    """Get an instance of the current Poetry project.
-
-    This is cached so that subsequent calls in the same invoke session reduce interactions with disk.
-    """
-    # Standard Library
-    from pathlib import Path
-
-    # Third Party
-    from poetry.core.factory import Factory
-
-    poetry = Factory().create_poetry(Path(".").resolve())
-    return poetry
-
-
-@lru_cache(maxsize=None)
-def poetry_project_name():
-    """Get the name of the current Poerty project."""
-    return poetry_project().pyproject.poetry_config["name"]
-
-
-@lru_cache(maxsize=None)
-def poetry_project_version():
-    """Get the version of the current Poerty project."""
-    return poetry_project().pyproject.poetry_config["version"]
-
-
-@lru_cache(maxsize=None)
-def poetry_wheel_builder():
-    """Get poetry WheelBuilder instance."""
-    # Third Party
-    from poetry.core.masonry.builders.wheel import WheelBuilder
-
-    builder = WheelBuilder(poetry_project())
-    return builder
-
-
-@lru_cache(maxsize=None)
-def poetry_wheelname():
-    """Get poetry properly formatted wheelname from WheelBuilder instance."""
-    builder = poetry_wheel_builder()
-    return builder.wheel_filename
 
 
 @lru_cache(maxsize=None)
