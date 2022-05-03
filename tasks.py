6f004550859d9b7b796db1df6d96d4017e3f51c1
Josh Peak <neozenith.dev@gmail.com>
2022-05-04 09:58:07 +1000

    Improve define-job documentation (#11)
    Co-authored-by: Josh Peak <josh.peak@86400.com.au>

--- tasks.py
+++ tasks.py
@@ -1,22 +1,16 @@
 # Third Party
 from invoke import task
-from invoke_common_tasks import build, ci, format, lint, test, typecheck  # noqa
+from invoke_common_tasks import *  # noqa
 
 # Our Libraries
-from invoke_databricks_wheel_tasks import (  # noqa
-    clean,
-    define_job,
-    reinstall,
-    runjob,
-    upload,
-)
+from invoke_databricks_wheel_tasks import *  # noqa
 
 # NOTE: Invoke tasks files don't support mypy typechecking for the forseeable future
 # They were looking at addressing it after Python2 EOL 01-01-2020 but there was a global pandemic.
 # https://github.com/pyinvoke/invoke/issues/357
 
 
-@task(pre=[build, upload, reinstall, runjob], default=True)
+@task(pre=[build, upload, reinstall, runjob], default=True)  # noqa
 def dev(c):
     """Default databricks flow."""
     ...
